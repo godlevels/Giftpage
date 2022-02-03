4da7e0abd248c18144a4ff1f56caf231fc9aaccd
CryptoVmD <93841754+CryptoVmD@users.noreply.github.com>
2022-02-03 09:07:06 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#ff6363',
+        secondary: {
+          100: '#e2e2d5',
+          200: '#888883'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
